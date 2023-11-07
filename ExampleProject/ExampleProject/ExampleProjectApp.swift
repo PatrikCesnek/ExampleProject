b879bb5af11d39d350c1a49485b19ce8cb302a0a
Patrik Cesnek <pcesnek290@gmail.com>
2023-11-07 16:47:21 +0100

    Innitial commit

---
+++ ExampleProject/ExampleProject/ExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleProjectApp.swift
+//  ExampleProject
+//
+//  Created by Patrik Cesnek on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
